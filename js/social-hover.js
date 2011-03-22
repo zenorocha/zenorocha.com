7e919141f3f75888603cb0e90bf9c9650f3a640a
Zeno Rocha <zno.rocha@gmail.com>
2011-03-22 17:08:42 -0300

    First commit of my personal website.

---
+++ js/social-hover.js
@@ -0,0 +1,29 @@
+$(document).ready(function()
+{
+	$("#social ul li").hover(function() { // Mouse over
+		$(this)
+			.stop().fadeTo(500, 1)
+			.siblings().stop().fadeTo(500, 0.2);
+			
+		$("a strong", this)
+			.stop()
+			.delay(200)
+			.animate({
+				opacity: 1,
+				top: "-10px"
+			}, 300);
+		
+	}, function() { // Mouse out
+		$(this)
+			.stop().fadeTo(500, 1)
+			.siblings().stop().fadeTo(500, 1);
+			
+		$("a strong", this)
+			.stop()
+			.animate({
+				opacity: 0,
+				top: "-1px"
+			}, 300);
+	});
+	
+});

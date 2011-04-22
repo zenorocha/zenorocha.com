2fadd3f1ec573c936c520d3cdb1300afa44dcfd4
Zeno Rocha <zno.rocha@gmail.com>
2011-04-21 23:08:40 -0300

    Changing the load of JSON data from PHP to JS

---
+++ js/load-projects.js
@@ -0,0 +1,9 @@
+(function($) {
+	
+	$.getJSON("data/projects.json",function(result){
+    	$.each(result.project, function(i, field){
+			$("#projects").append('<li class="alpha-ie"><a target="_blank" href="' + field.url + '">' + field.name + ' &lt;</a></li>');
+		});
+	});
+	
+})(jQuery);

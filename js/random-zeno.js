9c6f7a56dacb09a632147e143247b743e72257e1
Zeno Rocha <zno.rocha@gmail.com>
2011-04-21 23:29:23 -0300

    Random cartoon and changing icon from Linkedin to Github

---
+++ js/random-zeno.js
@@ -0,0 +1,6 @@
+$(document).ready(function()
+{
+	var img = ['zeno-java.png', 'zeno-html5.png', 'zeno-jquery.png', 'zeno-wp.png'];
+	$('footer').css({'background': 'url(img/zeno/' + img[Math.floor(Math.random() * img.length)] + ')'});
+	
+});

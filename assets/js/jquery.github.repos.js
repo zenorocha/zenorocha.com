078c06248ca7d95dd0eb7160cfdc09a953a6df77
Zeno Rocha <zno.rocha@gmail.com>
2013-01-15 10:06:30 -0200

    updating jquery.github.repos to version 0.2.2

--- assets/js/jquery.github.repos.js
+++ assets/js/jquery.github.repos.js
@@ -1,8 +1,10 @@
 /*
- *  Project: jQuery Github Repos
- *  Description: A jQuery plugin to display your Github Repositories.
- *  Author: Zeno Rocha
- *  License: MIT
+ *  jQuery Github Repos v0.2.2
+ *  A jQuery plugin to display your Github Repositories.
+ *  http://git.io/3A1RMg
+ *
+ *  Zeno Rocha
+ *  MIT License
  */
 
 // the semi-colon before function invocation is a safety net against concatenated
@@ -30,6 +32,7 @@
         this.element = element;
         this.$container = $(element);
         this.repo = this.$container.attr("data-repo");
+        this.cached;
 
         // jQuery has an extend method which merges the contents of two or
         // more objects, storing the result in the first object. The first object
@@ -45,42 +48,79 @@
 
     Plugin.prototype.init = function () {
 
-        var self = this;
+      var self = this;
+      var cached = sessionStorage.getItem('gh-repos:' + this.repo);
+
+      // Attempt to get cached repo data
+      if (window.sessionStorage && (cached != null)) {
+        self.applyTemplate(JSON.parse(cached));
+      }
+      else {
 
         $.ajax({
           url: 'https://api.github.com/repos/' + this.repo,
           dataType: 'jsonp',
           success: function(results){
 
-            var repo = results.data;
-            var date = new Date(repo.pushed_at);
-            var pushed_at = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
-            var $widget = $(' \
-              <div class="github-box">  \
-                  <div class="github-box-header"> \
-                      <h3> \
-                          <a href="' + repo.url.replace('api.','').replace('repos/','') + '">' + repo.name + '</a> \
-                      </h3> \
-                      <div class="github-stats"> \
-                          <a class="repo-watchers" href="' + repo.url.replace('api.','').replace('repos/','') + '/watchers">' + repo.watchers + '</a> \
-                          <a class="repo-forks" href="' + repo.url.replace('api.','').replace('repos/','') + '/forks">' + repo.forks + '</a> \
-                      </div> \
-                  </div> \
-                  <div class="github-box-content"> \
-                      <p>' + repo.description + ' &mdash; <a href="' + repo.url.replace('api.','').replace('repos/','') + '#readme">Read More</a></p> \
-                  </div> \
-                  <div class="github-box-download"> \
-                      <p class="repo-update">Latest commit to <strong>master</strong> on ' + pushed_at + '</p> \
-                      <a class="repo-download" href="' + repo.url.replace('api.','').replace('repos/','') + '/zipball/master">Download as zip</a> \
-                  </div> \
-              </div> \
-            ');
+            // Handle API failures
+            if (results.meta.status >= 400 && results.data.message){
+                console.warn(results.data.message);
+                return;
+            }
+            else {
 
-            $widget.appendTo(self.$container);
+              self.applyTemplate(results.data);
+
+              // Cache data
+              if (window.sessionStorage) {
+                sessionStorage.setItem('gh-repos:' + self.repo, JSON.stringify(results.data));
+              }
+
+            }
 
           }
-
         });
+
+
+
+      }
+
+    };
+
+    Plugin.prototype.applyTemplate = function (repo) {
+
+      var self = this;
+      var date = new Date(repo.pushed_at);
+      var pushed_at = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+
+      var $widget = $(' \
+        <div class="github-box">  \
+            <div class="github-box-header"> \
+                <h3> \
+                    <a href="' + repo.url.replace('api.','').replace('repos/','') + '">' + repo.name + '</a> \
+                </h3> \
+                <div class="github-stats"> \
+                    <a class="repo-watchers" href="' + repo.url.replace('api.','').replace('repos/','') + '/watchers">' + repo.watchers + '</a> \
+                    <a class="repo-forks" href="' + repo.url.replace('api.','').replace('repos/','') + '/forks">' + repo.forks + '</a> \
+                </div> \
+            </div> \
+            <div class="github-box-content"> \
+                <p>' + repo.description + ' &mdash; <a href="' + repo.url.replace('api.','').replace('repos/','') + '#readme">Read More</a></p> \
+            </div> \
+            <div class="github-box-download"> \
+                <p class="repo-update">Latest commit to <strong>master</strong> on ' + pushed_at + '</p> \
+                <a class="repo-download" href="' + repo.url.replace('api.','').replace('repos/','') + '/zipball/master">Download as zip</a> \
+            </div> \
+        </div> \
+      ');
+
+      self.appendTemplate($widget);
+
+    };
+
+    Plugin.prototype.appendTemplate = function ($widget) {
+      var self = this;
+      $widget.appendTo(self.$container);
     };
 
     // A really lightweight plugin wrapper around the constructor,

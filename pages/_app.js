3c5fe2157884c422f293c410f9d716cafac15679
Zeno Rocha <zno.rocha@gmail.com>
2021-10-30 03:02:23 -0700

    Remove keypress hook in favor of global kbar shortcuts

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import '../public/static/css/prism.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import Router from 'next/router'
-import useKeypress from '../lib/use-keypress'
-import sendEmail from '../lib/send-email'
 import * as gtag from '../lib/gtag'
 
 Router.events.on('routeChangeComplete', url => gtag.pageview(url))
@@ -13,8 +11,6 @@ const Noop = ({ children }) => children
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || Noop
-
-  useKeypress('e', sendEmail)
 
   return (
     <Layout>

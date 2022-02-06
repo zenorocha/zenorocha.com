25d5254aa7d4ebdaf8475462003a6f5b5b2011e2
Zeno Rocha <zno.rocha@gmail.com>
2022-02-06 11:26:30 -0800

    Improves wording

--- pages/investing.js
+++ pages/investing.js
@@ -41,13 +41,13 @@ function Investing(props) {
       <p dangerouslySetInnerHTML={{ __html: description }} />
 
       <h2>What I invest in</h2>
-      <p>I write $5-10k checks in <strong>developer-first startups</strong>. I prefer folks who are in early stages (think pre-seed or seed) and products that increase productivity, avoid task repetition, or improve performance.</p>
+      <p>I write $5-10k checks in <strong>developer-first startups</strong>. I prefer folks who are in early stages (think pre-seed or seed) and products that increase productivity, avoid task repetition, or unlock revenue potential.</p>
 
       <h2>What you get from me</h2>
       <p>I love helping with...</p>
       <ul>
         <li><strong>Product</strong> - being "user zero" for new features, providing feedback, and figuring out priorities.</li>
-        <li><strong>Branding</strong> - building a company brand that developers love, admire, and get inspired from.</li>
+        <li><strong>Branding</strong> - building a company brand that developers love, admire, and get inspired by.</li>
         <li><strong>Recruiting</strong> - leveraging my personal network to attract talented engineers and designers.</li>
         <li><strong>Enterprise</strong> - sharing my experience moving upmarket (what works and what doesn't).</li>
         <li><strong>Open Source</strong> - defining a strategy that will help with monetization and hiring.</li>
@@ -60,7 +60,7 @@ function Investing(props) {
       </div>
 
       <h2>Let's chat</h2>
-      <p>Hit me up if what you saw here makes sense to you.</p>
+      <p>Hit me up if what you read here resonates with you.</p>
     </div>
   )
 }

12dfa0a98f0ff7b5e56893c46c4b2a16fdb91da9
Zeno Rocha <zno.rocha@gmail.com>
2022-03-25 15:46:34 -0700

    Remove podcast dynamic data fetching

--- data/podcasts.js
+++ data/podcasts.js
@@ -1,3 +1,91 @@
+const bytetalk = [
+  {
+    date: '2021-08-25',
+    title: 'The Intersection Between Tech and Social Impact with Gary Carrier',
+    url: 'https://youtu.be/gfYBCmwN-8Y',
+  },
+  {
+    date: '2021-08-17',
+    title: 'The dark side of open source maintainability with Jonathan Neal',
+    url: 'https://youtu.be/J4bf2iEylTo',
+  },
+  {
+    date: '2021-08-11',
+    title: 'A/B Testing, Engineering Culture, and Developer Experience with Bruno Passos',
+    url: 'https://youtu.be/YiW-OukfQWU',
+  },
+  {
+    date: '2021-08-05',
+    title: 'Mechanical Keyboards, Streaming, and Pizza with Cassidy Williams',
+    url: 'https://youtu.be/E6TJmGsmRoE',
+  },
+  {
+    date: '2021-07-29',
+    title: 'How to start a tech podcast',
+    url: 'https://youtu.be/Qlv3EEi13zA',
+  },
+  {
+    date: '2021-07-21',
+    title: 'How to not mess up remote work',
+    url: 'https://youtu.be/2dT-JAuBIWM',
+  },
+  {
+    date: '2021-07-14',
+    title: 'Working at GitHub with Ramon Page',
+    url: 'https://youtu.be/p9d9GqTyleA',
+  },
+  {
+    date: '2021-07-08',
+    title: 'Navigating self-employment with Lena Sesardic',
+    url: 'https://youtu.be/483zOrSKCwM',
+  },
+  {
+    date: '2021-06-22',
+    title: 'Being a new parent in tech with Pedro Duarte',
+    url: 'https://youtu.be/jMQ_Rd8yqJM',
+  },
+  {
+    date: '2021-06-17',
+    title: 'The challenge of switching jobs with Demian Borba',
+    url: 'https://youtu.be/VzCPNx7mNjk',
+  },
+  {
+    date: '2021-06-08',
+    title: 'What does it mean to be Enterprise-Ready?',
+    url: 'https://youtu.be/CSe5YZoBWpY',
+  },
+  {
+    date: '2021-06-02',
+    title: 'Building Google documentation sites with Rob Dodson',
+    url: 'https://youtu.be/B0OwzEqPlAc',
+  },
+  {
+    date: '2021-05-25',
+    title: 'Productizing yourself with David Guttman',
+    url: 'https://youtu.be/WQeoSQ3fAt4',
+  },
+  {
+    date: '2021-05-18',
+    title: 'Building in Public and Salary Transparency',
+    url: 'https://youtu.be/U7sGg-BNKEo',
+  },
+  {
+    date: '2021-05-11',
+    title: 'Creating StackOverflow and Discourse with Jeff Atwood',
+    url: 'https://youtu.be/oZz2Rr5qFuQ',
+  },
+  {
+    date: '2021-05-11',
+    title: 'Integrating thousands of systems at scale with Netto Farah',
+    url: 'https://youtu.be/FF3bIUOfhnk',
+  },
+  {
+    date: '2021-05-11',
+    title: 'Introduction, Automation, and SaaS',
+    url: 'https://youtu.be/d2jUY_jQK14',
+  },
+]
+
 const appearances = [
   {
     date: '2021-03-29',
@@ -131,4 +219,4 @@ const zofe = [
   },
 ]
 
-export { appearances, zofe }
+export { bytetalk, appearances, zofe }
--- pages/podcasts.js
+++ pages/podcasts.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Base from '../layouts/Base'
 import stripHtml from '../lib/strip-html'
-import { appearances, zofe } from '../data/podcasts'
+import { bytetalk, appearances, zofe } from '../data/podcasts'
 import ListItem from '../components/ListItem'
 import { ListGroup } from '../components/ListGroup'
 import { AnimateSharedLayout } from 'framer-motion'
@@ -15,26 +15,7 @@ export async function getStaticProps() {
     secondaryColor: 'purple',
   }
 
-  const { TRANSISTOR_API_KEY } = process.env
-
-  if (!TRANSISTOR_API_KEY) {
-    return { props: { ...meta } }
-  }
-
-  const payload = {
-    headers: {
-      'x-api-key': TRANSISTOR_API_KEY,
-    },
-  }
-
-  const req = await fetch(
-    'https://api.transistor.fm/v1/episodes?show_id=19825&status=published',
-    payload
-  )
-  const res = await req.json()
-  const episodes = res.data
-
-  return { props: { ...meta, episodes }, revalidate: 60 }
+  return { props: { ...meta }, revalidate: 60 }
 }
 
 function Podcasts(props) {
@@ -60,25 +41,7 @@ function Podcasts(props) {
       })
   }
 
-  const renderByteTalk = () => {
-    const { episodes } = props
-
-    if (episodes) {
-      return episodes.map((episode, index) => {
-        return (
-          <ListItem
-            key={index}
-            index={index}
-            href={`https://bytetalkpodcast.com/${episode.attributes.number}`}
-            title={episode.attributes.title}
-            date={episode.attributes.published_at}
-          />
-        )
-      })
-    }
-  }
-
-  const renderOther = items => {
+  const renderEpisode = items => {
     return items.map((item, index) => {
       return (
         <ListItem
@@ -117,20 +80,20 @@ function Podcasts(props) {
           A podcast where Jonni and I interview the most productive people in
           tech.
         </p>
-        <ListGroup>{renderByteTalk()}</ListGroup>
+        <ListGroup>{renderEpisode(bytetalk)}</ListGroup>
 
         <h2>Appearances</h2>
         <p>
           This is the list of all the podcasts that I gave an interview so far.
         </p>
-        <ListGroup>{renderOther(appearances)}</ListGroup>
+        <ListGroup>{renderEpisode(appearances)}</ListGroup>
 
         <h2>Zone Of Front-Enders</h2>
         <p>
           My first podcast, ZOFE, where Daniel and I talked about web
           technologies.
         </p>
-        <ListGroup>{renderOther(zofe)}</ListGroup>
+        <ListGroup>{renderEpisode(zofe)}</ListGroup>
       </AnimateSharedLayout>
     </>
   )

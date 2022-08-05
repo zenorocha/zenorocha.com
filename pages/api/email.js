9c0c1c21dd0bffb13a7c27ba6db852671b883579
Zeno Rocha <zno.rocha@gmail.com>
2022-08-04 20:02:48 -0700

    Replace API call with Klotty SDK

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,21 +1,17 @@
+import Klotty from 'klotty'
+const klotty = new Klotty(process.env.KLOTTY_API_KEY)
+
 export default async function sendEmail(req, res) {
   try {
     const { name, email, message } = req.body
 
-    await fetch('https://api.klotty.com/email', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${process.env.KLOTTY_API_KEY}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        from: 'website@zenorocha.com',
-        to: 'zno.rocha@gmail.com',
-        subject: `${name} - via zenorocha.com`,
-        html: `<b>Name:</b> ${name}<br />
+    await klotty.sendEmail({
+      from: 'website@zenorocha.com',
+      to: 'zno.rocha@gmail.com',
+      subject: `${name} - via zenorocha.com`,
+      html: `<b>Name:</b> ${name}<br />
 <b>Email:</b> ${email}<br />
 <b>Message:</b> ${message}<br />`
-      })
     })
 
     res.status(200).json({ message: 'Email sent' })

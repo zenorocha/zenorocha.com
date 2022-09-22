3ff9bf66c572e5926dfabd980447c6f354454d5e
Zeno Rocha <zno.rocha@gmail.com>
2022-09-22 16:48:05 -0700

    integrate first react-email component :tada:

--- components/EmailTemplate.js
+++ components/EmailTemplate.js
@@ -1,13 +1,15 @@
+import { Html } from '@react-email/html';
+
 export default function EmailTemplate(props) {
   const { name, email, message } = props
 
   return (
-    <div>
+    <Html>
       <ul>
         <li><strong>Name:</strong>{' '}{name}</li>
         <li><strong>Email:</strong>{' '}{email}</li>
         <li><strong>Message:</strong>{' '}{message}</li>
       </ul>
-    </div>
+    </Html>
   )
 }

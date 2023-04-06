1895f529439ab3662ff52122df6ed9db09716d8c
Zeno Rocha <zno.rocha@gmail.com>
2023-04-06 10:32:02 -0700

    add React on the Beach talk

--- data/talks.js
+++ data/talks.js
@@ -1,5 +1,17 @@
 const items = [
   {
+    year: '2023',
+    summary: '',
+    talks: [
+      {
+        title: 'React on the Beach',
+        url: 'https://reactonthebeach.com',
+        date: '2023-09-07',
+        where: 'Amsterdam - Netherlands'
+      },
+    ]
+  },
+  {
     year: '2022',
     summary: '',
     talks: [

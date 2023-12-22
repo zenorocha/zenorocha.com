c55ecb57e92e94c9c84408beafc3f8dc63669a3b
Zeno Rocha <zno.rocha@gmail.com>
2023-12-22 10:41:53 -0300

    refactor json-ld

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import ShortcutHome from '../components/ShortcutHome'
 import { PostMain, PostContent, PostContainer } from '../components/Post'
 import { Wrapper } from '../components/Wrapper'
+import { getPersonJsonLd } from '../lib/json-ld'
 
 export async function getStaticProps() {
   return {
@@ -19,117 +20,6 @@ export async function getStaticProps() {
 export default function Index(props) {
   const { title, description, image } = props
 
-  const getPersonJsonLd = () => {
-    return {
-      __html: `{
-        "@context": "https://schema.org/",
-        "@type": "Person",
-        "url": "https://zenorocha.com/",
-        "affiliation": [
-          {
-            "@type": "Organization",
-            "@id": "https://www.wikidata.org/wiki/Q123921042",
-            "url": "https://resend.com/",
-            "name": "Resend"
-          },
-          {
-            "@type": "Organization",
-            "@id": "https://www.wikidata.org/wiki/Q2616400",
-            "url": "https://www.ycombinator.com/",
-            "name": "Y Combinator"
-          }
-        ],
-        "description": "Zeno Rocha is a Brazilian software entrepreneur who is the founder and CEO of Resend. Before founding Resend, Zeno was a VP of Developer Experience at WorkOS.",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/9/90/Zeno_Rocha.png",
-        "name": "Zeno Rocha",
-        "givenName": "Zeno",
-        "familyName": "Rocha",
-        "gender": "Male",
-        "birthPlace": "Curitiba",
-        "jobTitle": "Founder and CEO",
-        "sameAs": [
-          "https://www.wikidata.org/wiki/Q123921198",
-          "https://www.linkedin.com/in/zenorocha",
-          "https://twitter.com/zenorocha",
-          "https://www.crunchbase.com/person/zeno-rocha",
-          "https://www.reddit.com/user/zenorocha",
-          "https://www.instagram.com/zenorocha",
-          "https://github.com/zenorocha"
-        ],
-        "knowsAbout": [
-          {
-            "@type": "Organization",
-            "@id": "https://www.wikidata.org/wiki/Q123921042",
-            "name": "Resend"
-          },
-          {
-            "@type": "Thing",
-            "@id": "https://www.wikidata.org/wiki/Q80993",
-            "name": "Software Engineering"
-          },
-          {
-            "@type": "Thing",
-            "@id": "https://www.wikidata.org/wiki/Q1254596",
-            "name": "Software as a Service"
-          },
-          {
-            "@type": "Organization",
-            "@id": "https://www.wikidata.org/wiki/Q2616400",
-            "name": "Y Combinator"
-          }
-        ],
-        "knowsLanguage": [
-          {
-            "@type": "Language",
-            "@id": "https://www.wikidata.org/wiki/Q750553",
-            "name": "Brazilian Portuguese"
-          },
-          {
-            "@type": "Language",
-            "@id": "https://www.wikidata.org/wiki/Q1860",
-            "name": "English"
-          }
-        ],
-        "nationality": [
-          {
-            "@type": "Country",
-            "@id": "https://www.wikidata.org/wiki/Q155",
-            "name": "Brazil"
-          },
-          {
-            "@type": "Country",
-            "@id": "https://www.wikidata.org/wiki/Q30",
-            "name": "United States of America"
-          }
-        ],
-        "alumniOf": [
-          {
-            "@type": "EducationalOrganization",
-            "@id": "https://www.wikidata.org/wiki/Q2616400",
-            "name": "Y Combinator",
-            "url": "https://www.ycombinator.com/",
-            "startDate": "2023",
-            "endDate": "2023"
-          },
-          {
-            "@type": "EducationalOrganization",
-            "@id": "https://www.wikidata.org/wiki/Q3011938",
-            "name": "Federal University of the State of Rio de Janeiro",
-            "url": "https://www.unirio.br/",
-            "startDate": "2009",
-            "endDate": "2013",
-            "major": [
-              {
-                "@type": "DefinedTerm",
-                "name": "Information Systems"
-              }
-            ]
-          }
-        ]
-      }`,
-    };
-  }
-
   return (
     <Wrapper>
       <Head>
@@ -141,7 +31,9 @@ export default function Index(props) {
         <meta content={`https://zenorocha.com${image}`} property="og:image" />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={getPersonJsonLd()}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(getPersonJsonLd())
+          }}
           key="person-jsonld"
         />
       </Head>

368a3caea8515ad6f2782eb85d94237e669cada5
luxonauta <luxon4uta@gmail.com>
2026-01-12 16:37:29 -0300

    📐 fix alignment on error pages

--- app/error.js
+++ app/error.js
@@ -1,12 +1,25 @@
 'use client';
 
-import ErrorMessage from '../components/ErrorMessage';
-import Blogpost from '../layouts/Blogpost';
+import { Box } from '../components/Box';
+import Navbar from '../components/Navbar';
+import { PostMain } from '../components/Post';
+import { PostContainer } from '../components/Post';
+import ShortcutError from '../components/ShortcutError';
+import { Wrapper } from '../components/Wrapper';
 
 export default function ErrorBoundary() {
   return (
-    <Blogpost title="" image={null} date={null}>
-      <ErrorMessage />
-    </Blogpost>
+    <Wrapper>
+      <Navbar />
+      <PostMain>
+        <PostContainer>
+          <Box className="text-center">
+            <h1>five hundred</h1>
+            <ShortcutError />
+            <p>Something isn&apos;t right.</p>
+          </Box>
+        </PostContainer>
+      </PostMain>
+    </Wrapper>
   );
 }
